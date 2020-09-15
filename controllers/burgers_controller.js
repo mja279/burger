cae7408a5f68bc3e2f97042516b189f90afd5fad
Maria Jimena <mja279@georgetown.edu>
2020-09-14 23:32:40 -0400

    pushing updated code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,7 @@ router.post("/api/burger", function(req, res) {
   burger.create([
     "burger_name", "devour"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.devour
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -33,7 +33,7 @@ router.put("/api/burger/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+    devour: req.body.devour
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
